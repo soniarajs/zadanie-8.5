199a92150593214dbebbdbe3a188463528c15d1d
sonia <sonia@gmail.com>
2017-04-03 23:47:31 +0200

    funkcje

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
-var a = 3;
-	b = 1;
-	value = (a * a) + (2 * a * b) - (b * b);
 
-console.log('Wynik wynosi' + value);
+var a  = ,
+    h  = ;
 
-if (value >= 0) {
-	console.log('wynik dodatni');
-}
-if (value < 0) {
-	console.log('wynik ujemny');
+function getTriangleArea (a, h) {
+
+        a*h/2
+
+
+	if (a > 0) {
+	console.log('Dane ok');
+	}
+	if (h > 0) {
+	console.log('Dane ok');
+	}
+	else {
+	console.log('Nieprawidłowe dane');
+	}
+
+	return 'wartośćDzialania';
+	var wartośDzialania = getTriangleArea(a, h);
 }
 
-switch (value) {
-    case '0': 
-    	console.log('wynik jest równy 0');
-        break;
-    default:
-    	console.log('wynik nie jest równy 0');
-}
+console.log(getTriangleArea(10,6))
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(7, 1);
+var triangle3Area = getTriangleArea(-4, 0);
